a604f961d38cd7ebcd56325b1e0a5d6334479324
Matheus Carreirão Cantanhede <matheus91cc@gmail.com>
2025-10-31 12:52:00 -0300

    adding tower logic commentaries

--- Assets/_Tower Climbing/_Scripts/TowerManager.cs
+++ Assets/_Tower Climbing/_Scripts/TowerManager.cs
@@ -13,63 +13,67 @@ public class TowerManager : MonoBehaviour {
     [SerializeField]
     private Transform[] platformSpawns;
     [SerializeField]
-    private float spawnRate;
+    private float spawnPlatRate;
     [Space(10)]
 
     [SerializeField]
     private Transform towerBase = null;
+    [SerializeField]
+    private float rotationTime = 0.15f;
     [Space(10)]
 
     [Header("Floor Reference")]
     public GameObject floor;
 
-    private float timer;
+    private float platSpawnTimer;
     private int randomPos;
-    private int[] platRotations = { 0, 90, 180, 270 };
+    private int[] platDefaultRotations = { 0, 90, 180, 270 };
     private int cachedRandomRotationIndex;
     private bool isTowerRotating;
-    private float rotationTarget;
     private float elapsed;
-    private float startRotation;
-    private float endRotation;
+    private float startYRotation;
+    private float endYRotation;
 
     private void Awake () 
     {
-        timer = spawnRate;		
+        platSpawnTimer = spawnPlatRate;		
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        timer -= Time.deltaTime;
-        if(timer <= 0)
+        platSpawnTimer -= Time.deltaTime;
+        if(platSpawnTimer <= 0)
         {
             randomPos = UnityEngine.Random.Range(0, platformSpawns.Length);
             SpawnPlat();
-            timer = spawnRate;
+            platSpawnTimer = spawnPlatRate;
         }
-       
+        
+
+        // Lerp tower euler angle 
         if (isTowerRotating)
         {
             elapsed += Time.deltaTime;
-            float interpolation = Mathf.Clamp01(elapsed / 0.15f);
+            float interpolation = Mathf.Clamp01(elapsed / rotationTime);
 
-            // Interpolate only the Y angle (with wrap-around support)
-            float currentY = Mathf.LerpAngle(startRotation, endRotation, interpolation);
+            // Interpolate only the Y angle
+            float currentY = Mathf.LerpAngle(startYRotation, endYRotation, interpolation);
 
-            // Keep current X and Z unchanged
+            // Set new Vector3 with new euler angles
             Vector3 currentEuler = transform.eulerAngles;
             currentEuler.y = currentY;
 
+            // Set tower with new euler angles
             transform.rotation = Quaternion.Euler(currentEuler);
 
+            // Stop interpolation if rotation has been completed
             if (interpolation >= 1f)
             {
                 isTowerRotating = false;
                 elapsed = 0;
             }
         }
-    
     }
 
     public void RotateTower(bool isLeftRotation)
@@ -78,27 +82,15 @@ public class TowerManager : MonoBehaviour {
         {
             isTowerRotating = true;
 
-            startRotation = this.transform.localEulerAngles.y;
-            endRotation = startRotation - 90;
-
-            //Vector3 towerRotation = this.transform.localEulerAngles;
-            //towerRotation.y -= 90;
-            //rotationTarget = towerRotation.y -= 90;
-            //this.transform.localEulerAngles = towerRotation;
+            startYRotation = this.transform.localEulerAngles.y;
+            endYRotation = startYRotation - 90;
         }
         else
         {
             isTowerRotating = true;
 
-            startRotation = this.transform.localEulerAngles.y;
-            endRotation = startRotation + 90;
-
-            //Vector3 towerRotation = this.transform.localEulerAngles;
-            //rotationTarget = towerRotation.y += 90;
-
-            //towerRotation.y += 90;
-            //this.transform.localEulerAngles = towerRotation;
-
+            startYRotation = this.transform.localEulerAngles.y;
+            endYRotation = startYRotation + 90;
         }
     }
 
@@ -111,16 +103,18 @@ public class TowerManager : MonoBehaviour {
     private void SpawnPlat()
     {
         const int MAX_TRIES = 99;
+        int randomRotation = -1;
 
-        int randomIndex = -1;
-
+        // Before to spawn a new platform, it is necessary to avoid to use the same rotation,
+        // as the last spawned platform, to avoid dead locks.
         for (int i = 0; i < MAX_TRIES; i++)
         {
-            randomIndex = UnityEngine.Random.Range(0, platRotations.Length);
+            randomRotation = UnityEngine.Random.Range(0, platDefaultRotations.Length);
 
-            if(randomIndex != cachedRandomRotationIndex)
+            // Use a different rotation value from last one cached
+            if(randomRotation != cachedRandomRotationIndex)
             {
-                cachedRandomRotationIndex = randomIndex;
+                cachedRandomRotationIndex = randomRotation;
                 break;
             }
         }
@@ -130,7 +124,7 @@ public class TowerManager : MonoBehaviour {
         platform.transform.SetParent(towerBase.transform, true);
         
         Vector3 platRotation = platform.transform.localEulerAngles;
-        platRotation.y = platRotations[randomIndex];
+        platRotation.y = platDefaultRotations[randomRotation];
         platform.transform.localEulerAngles = platRotation;
 
     }
